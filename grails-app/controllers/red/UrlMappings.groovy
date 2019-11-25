96182693509e3abe63b2ca09fba2441803028887
Maximiliano Peña <maxipenia@gmail.com>
2019-11-24 23:35:38 -0300

    Redirects para hacerme feliz.

--- grails-app/controllers/red/UrlMappings.groovy
+++ grails-app/controllers/red/UrlMappings.groovy
@@ -12,5 +12,7 @@ class UrlMappings {
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
+        "/dbconsole"(redirect:'/h2-console')
+        "/db"(redirect: '/h2-console')
     }
 }
