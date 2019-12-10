e5921f8f7ce50e7369a4cc8028f302ecb9def6db
Maximiliano Peña <maxipenia@gmail.com>
2019-12-09 22:55:15 -0300

    Definido terreno.

--- grails-app/domain/red/Terreno.groovy
+++ grails-app/domain/red/Terreno.groovy
@@ -1,7 +1,21 @@
 package red
 
+import UserType.MoneyUserType
+import org.joda.money.Money
+
 class Terreno {
 
+    String direccion
+    BigDecimal superficie
+    Money costo
+
+    static embedded  =  ['costo']
+
+    static mapping = {
+        costo type: MoneyUserType, column: 'COSTO'
+    }
+
     static constraints = {
+        direccion blank: false
     }
 }
